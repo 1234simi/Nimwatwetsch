81a826de038312827007b650da6d33dd07781875
Simi <simi_1991@gmx.ch>
2022-10-24 16:35:17 +0200

    input geadded

---
+++ Taschenrechner.py
@@ -0,0 +1,12 @@
+
+
+### Die Zahlen und das Operations-Zeichen wird eingegeben
+zahl_1 = input("Bitte gib die Erste Zahl ein: ")
+zeichen = input("Bitte gib das Operations-Zeichen ein: ")
+zahl_2 = input("Bitte gib die Zweite Zahl ein: ")
+
+
+### Die Eingaben werden ausgegeben
+print("Zahl 1: ", zahl_1)
+print("Das Operations-Zeichen ist: ", zeichen)
+print("Zahl 2: ", zahl_2)
