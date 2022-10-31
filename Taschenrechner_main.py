940a4d919e6d1b97980a472fe94c1d97993f4ace
Simi <simi:1991@gmx.ch>
2022-10-31 14:52:20 +0100

    bug fixes

--- Taschenrechner_main.py
+++ Taschenrechner_main.py
@@ -17,10 +17,10 @@ def eingaben_machen():
     print("Operations-Zeichen: ", zeichen)
 
     ## es wird die erste Zahl geprüft 
-    zahl_1 = zahl_1_valid(zahl_1)
+    # zahl_1 = zahl_1_valid(zahl_1)
 
     # Die beiden Zahlen und das Operations_Symbol werden zurückgegeben
-    return zahl_1, zeichen, zahl_2
+    return float(zahl_1), zeichen, float(zahl_2)
 
 
 
@@ -47,7 +47,7 @@ def operations_zeichen_valid(zeichen_liste_real):
         ## Zeichen in ASCII-Code umwandeln
         ascii_zeichen = int(zeichen_liste_real[0])
         if ascii_zeichen == 42 or ascii_zeichen == 43 or ascii_zeichen == 45 or ascii_zeichen == 47:
-            zeichen = int(zeichen_liste_real[0])
+            zeichen = ascii_zeichen
             return zeichen
         else:
             print(f"\tDas Operations-Zeichen ist nicht valide!")
@@ -58,8 +58,8 @@ def operations_zeichen_valid(zeichen_liste_real):
     if (len(zeichen_liste_real) >= 1):
         pass
 
-def zahl_1_valid(zahl1):
-    zahlvalid = zahl1
+# def zahl_1_valid(zahl1):
+#     zahlvalid = zahl1
 
 
 
@@ -80,6 +80,9 @@ def division(zahl_1, zahl_2):
 
 def berechnungen_machen(zahl_1, zeichen, zahl_2):
 
+    print(f"zahl_1 = {zahl_1}")
+    print(f"zahl_2 = {zahl_2}")
+    print(f"Zeichen = {zeichen}")
     ### Operstionen auswählen:
     if (zeichen == 42):
         result = multiplikation(zahl_1, zahl_2)
@@ -100,7 +103,7 @@ def berechnungen_machen(zahl_1, zeichen, zahl_2):
     return result
 
 def ausgabe_trenner(zeichen):
-
+    print(f"Zeichen in ausgabe_trenner = {zeichen}")
     if zeichen == 43:
         trenner = "++"
     elif zeichen == 45:
