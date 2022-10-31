70e5c2230eda65a078afea80b7b7ecc84455c444
Simi <simi:1991@gmx.ch>
2022-10-31 16:46:21 +0100

    test_TR_eingabe_machen in class gesetzt

--- test_Taschenrechner_eingabe_machen.py
+++ test_Taschenrechner_eingabe_machen.py
@@ -1,40 +1,29 @@
 import pytest
+import Taschenrechner_main as tr
 from Taschenrechner_main import operations_zeichen_valid
 from Taschenrechner_main import zahl_1_valid
-
 import unittest
 
 
 class TestEingabe(unittest.TestCase):
-
     def test_opz_valid_one_char(self):
-        zeichen = [43]
-        assert operations_zeichen_valid(zeichen) == 43
-        zeichen = [45]
-        assert operations_zeichen_valid(zeichen) == 45
-        zeichen = [42]
-        assert operations_zeichen_valid(zeichen) == 42
-        zeichen = [47]
-        assert operations_zeichen_valid(zeichen) == 47
-
+        self.assertEqual(tr.operations_zeichen_valid([43]), 43)
+        self.assertEqual(tr.operations_zeichen_valid([45]), 45)
+        self.assertEqual(tr.operations_zeichen_valid([42]), 42)
+        self.assertEqual(tr.operations_zeichen_valid([47]), 47)
+    def test_opz_invalid_one_char(self):
+        self.assertNotEqual(tr.operations_zeichen_valid([43]), 42)
+        self.assertNotEqual(tr.operations_zeichen_valid([45]), 47)
+        self.assertNotEqual(tr.operations_zeichen_valid([42]), 45)
+        self.assertNotEqual(tr.operations_zeichen_valid([47]), 126)
+    def test_opz_valide_two_char(self):
+        zeichen = [47, 47]
+        assert operations_zeichen_valid(zeichen) == 126
 
 # ascii()
 #     +    -   *   /
 #     43, 45, 42, 47
 
-
-
-    def test_opz_invalid_one_char(self):
-        zeichen = [43]
-        assert not operations_zeichen_valid(zeichen) == 42
-        zeichen = [45]
-        assert not operations_zeichen_valid(zeichen) == 47
-        zeichen = [42]
-        assert not operations_zeichen_valid(zeichen) == 45
-        zeichen = [47]
-        assert not operations_zeichen_valid(zeichen) == 126
-
-    def test_opz_valide_two_char(selfself):
 
 
 
