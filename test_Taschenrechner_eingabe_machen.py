ae67ce1973cd85d24c7ac80957f4775a348804b6
DodoMels <107034626+DodoMels@users.noreply.github.com>
2022-10-31 16:28:53 +0100

    monkeypatch faild

--- test_Taschenrechner_eingabe_machen.py
+++ test_Taschenrechner_eingabe_machen.py
@@ -27,6 +27,13 @@ def test_num_1_valid():
     assert zahl_1_valid(zahl1) == '067489367'
     zahl1 = '00000000000'
     assert zahl_1_valid(zahl1) == '00000000000'
-def test_num_1_invalid():
-    zahl1 = 'gh'
-    assert not zahl_1_valid(zahl1) == '2'
+
+def read_two_strings():
+    str1 = input('abd')
+    str2 = input('234')
+    return str1, str2
+def test_read_two_strings_valid(monkeypatch):
+    monkeypatch.setattr('zahl_1_valid',io.StringIO('first\nsecond'))
+    str1,str2 = read_two_strings()
+    assert str1 == 'first'
+    assert str2 == 'second'
